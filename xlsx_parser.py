a4f67d8bd4158caff22a3736fd2b7123e72babe8
Ivan Kinder <ivankinder11@gmail.com>
2020-09-15 16:39:28 +0300

    Парсер excel

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -11,15 +11,15 @@ for i in range(2, data_sheet.max_row):
     if data_sheet.cell(row=i, column=32).value == '':
         continue
     id_in_data[data_sheet.cell(row=i, column=32).value] = (
-    data_sheet.cell(row=i, column=1).value, data_sheet.cell(row=i, column=34).value,
-    data_sheet.cell(row=i, column=11).value, data_sheet.cell(row=i, column=10).value, [])
+        data_sheet.cell(row=i, column=1).value, data_sheet.cell(row=i, column=34).value,
+        data_sheet.cell(row=i, column=11).value, data_sheet.cell(row=i, column=10).value, [])
 
 id_in_classes = {}
 for i in range(2, classes_sheet.max_row):
     if classes_sheet.cell(row=i, column=3).value == '':
         continue
     id_in_classes[classes_sheet.cell(row=i, column=3).value] = (
-    classes_sheet.cell(row=i, column=1).value, classes_sheet.cell(row=i, column=4).value)
+        classes_sheet.cell(row=i, column=1).value, classes_sheet.cell(row=i, column=4).value)
 
 id_in_rooms = []
 for i in range(2, rooms_sheet.max_row):
@@ -32,8 +32,7 @@ data_sheet['BA1'].value = 'Верный класс'
 for i in range(2, data_sheet.max_row + 1):
     if data_sheet.cell(row=i, column=32).value not in id_in_classes.keys():
         data_sheet.cell(row=i, column=53).value = 'Нет такого корпуса'
-    elif id_in_data[data_sheet.cell(row=i, column=32).value][:1] == id_in_classes[
-        data_sheet.cell(row=i, column=32).value]:
+    elif id_in_data[data_sheet.cell(row=i, column=32).value][:1] == id_in_classes[data_sheet.cell(row=i, column=32).value]:
         data_sheet.cell(row=i, column=53).value = id_in_classes[data_sheet.cell(row=i, column=32).value][1]
     else:
         data_sheet.cell(row=i, column=53).value = id_in_classes[data_sheet.cell(row=i, column=32).value][1]
@@ -47,16 +46,18 @@ for value in id_in_data.values():
 
 data_sheet['BB1'].value = 'Верная комнатность'
 for i in range(2, data_sheet.max_row + 1):
-    if id_in_data[data_sheet.cell(row=i, column=32).value][4]:
-        if len(id_in_data[data_sheet.cell(row=i, column=32).value][4]) == 1:
-            data_sheet.cell(row=i, column=54).value = ', '.join(id_in_data[data_sheet.cell(row=i, column=32).value][4])
+    if data_sheet.cell(row=i, column=32).value != '':
+        if id_in_data[data_sheet.cell(row=i, column=32).value][4]:
+            if len(id_in_data[data_sheet.cell(row=i, column=32).value][4]) == 1:
+                data_sheet.cell(row=i, column=54).value = ', '.join(
+                    id_in_data[data_sheet.cell(row=i, column=32).value][4])
+            else:
+                data_sheet.cell(row=i, column=54).value = 'Пересечение данных: ' + ', '.join(
+                    id_in_data[data_sheet.cell(row=i, column=32).value][4])
         else:
-            data_sheet.cell(row=i, column=54).value = 'Пересечение данных: ' + ', '.join(
-                id_in_data[data_sheet.cell(row=i, column=32).value][4])
-    else:
-        if data_sheet.cell(row=i, column=11).value:
-            data_sheet.cell(row=i, column=54).value = 'Нет ID Корпуса или данных по такой площади'
-        else:
-            data_sheet.cell(row=i, column=54).value = 'Не хватает данных (комнатности)'
+            if data_sheet.cell(row=i, column=11).value:
+                data_sheet.cell(row=i, column=54).value = 'Нет ID Корпуса или данных по такой площади'
+            else:
+                data_sheet.cell(row=i, column=54).value = 'Не хватает данных (комнатности)'
 
 wb.save('DDY.xlsx')
