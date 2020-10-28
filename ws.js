37283e1569582510257b0ce8157f559d1ab80aa7
Ivan Kinder <ivankinder11@gmail.com>
2020-10-28 10:38:04 +0300

    messenger.html

---
+++ ws.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let socket = new WebSocket("ws://localhost:5678");
+
+socket.onopen = function () {
+    document.querySelector("textarea").addEventListener('keyup', function (e) {
+        if (e.keyCode === 13) {
+            if (this.value.trim() === "") {
+                return false;
+            }
+            socket.send(this.value.trim());
+            this.value = "";
+        }
+    });
+};
+
+socket.onerror = function () {
+    console.log('Ошибка при подключении');
+};
+
+let p = "";
+socket.onmessage = function (e) {
+    p = document.createElement("p");
+    p.innerHTML = e.data;
+    document.querySelector(".message").appendChild(p);
+};
