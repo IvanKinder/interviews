09b3f7518619883c356d357eb1c37dfb92298283
Ivan Kinder <ivankinder11@gmail.com>
2020-12-16 13:50:16 +0300

    Редактирование теперь открыто только админу

--- test_api/mainapp/views.py
+++ test_api/mainapp/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from rest_framework import generics
 from mainapp.permissions import IsAdmin
 
@@ -8,25 +10,40 @@ from mainapp.serializers import QuestionDetailSerializer, PollDetailSerializer, 
 
 class QuestionCreateView(generics.CreateAPIView):
     serializer_class = QuestionDetailSerializer
-    # permission_classes = (IsAdmin, )
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class PollCreateView(generics.CreateAPIView):
     serializer_class = PollDetailSerializer
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class QuestionListView(generics.ListAPIView):
     serializer_class = QuestionListSerializer
     queryset = Question.objects.all()
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class PollListView(generics.ListAPIView):
     serializer_class = PollListSerializer
     queryset = Poll.objects.all()
 
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class QuestionDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = QuestionDetailSerializer
     queryset = Question.objects.all()
 
-
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
--- test_api/test_api/urls.py
+++ test_api/test_api/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/mainapp/', include('mainapp.urls')),
+    path('accounts/', include('rest_framework.urls')),
 ]
