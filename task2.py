6d9c1238c98b549ec2da749c80f99145f5071d29
Ivan Kinder <ivankinder11@gmail.com>
2021-03-19 15:21:43 +0300

    tetrika task 2 done

--- task2.py
+++ task2.py
@@ -9,34 +9,29 @@ DICT_NUMS = {}
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту")
 
-
-i = 0
 flag = True
+
 while flag:
     try:
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         html_animals_list = soup.findAll('li')
         for tag in html_animals_list[2:202]:
-            if ord(tag.text[0].lower()) >= ord('я'):
+            if ord(tag.text[0].lower()) > 1105:
                 continue
             if tag.text[0] == 'A':
                 flag = False
             ANIMALS_LIST.append(tag.text)
         driver.find_element_by_link_text('Следующая страница').click()
-        i += 1
     except:
         break
 
-
-letter = ord('а')
-
-
 for animal in ANIMALS_LIST:
-    if 1072 <= ord(animal[0].lower()) <= 1103:
+    if 1072 <= ord(animal[0].lower()) <= 1105:
         DICT_NUMS[animal[0]] = []
 
 for animal in ANIMALS_LIST:
-    DICT_NUMS[animal[0]].append(animal)
+    if 1072 <= ord(animal[0].lower()) <= 1105:
+        DICT_NUMS[animal[0]].append(animal)
 
 for key, value in DICT_NUMS.items():
     print(f'{key}: {len(value)}')
