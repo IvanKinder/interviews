ccce2af8c6cf7a097a7bc631c6be2bdbcf7da110
Ivan Kinder <ivankinder11@gmail.com>
2021-04-25 22:00:56 +0300

    buttons

--- table/static/frontend/App.js
+++ table/static/frontend/App.js
@@ -8,59 +8,81 @@ Ext.onReady(function () {
         autoLoad: true,
         proxy: {
             type: 'rest',
-            url: 'http://127.0.0.1:8080/books/',
+            url: 'http://127.0.0.1:8000/books/',
             reader: {
-                type: 'json'
+                type: 'json',
+                root: 'data'
+            },
+            writer: {
+                type: 'json',
+                root: 'data'
             }
         }
     });
 
+    var Editing = Ext.create('Ext.grid.plugin.RowEditing');
+
+
     Ext.create('Ext.grid.Panel', {
         title: 'Books',
+        plugins: [Editing],
         height: 900,
-        width: 1400,
+        width: 600,
         store: store,
         columns: [{
             header: 'name',
-            dataIndex: 'name'
+            dataIndex: 'name',
+            field: {
+                xtype: 'textfield'
+            }
         }, {
             header: 'author',
-            dataIndex: 'author'
+            dataIndex: 'author',
+            field: {
+                xtype: 'textfield'
+            }
         }, {
             header: 'year',
-            dataIndex: 'year'
+            dataIndex: 'year',
+            field: {
+                xtype: 'textfield'
+            }
         }, {
             header: 'in_stock',
-            dataIndex: 'in_stock'
+            dataIndex: 'in_stock',
+            field: {
+                xtype: 'textfield'
+            }
         }, {
             header: 'price',
-            dataIndex: 'price'
-        }],
-        renderTo: Ext.getBody()
-    });
-
-    // Ext.create('Ext.Button', {
-    //     text: 'Click me',
-    //     margin: '-800 0 0 600',
-    //     height: 90,
-    //     width: 140,
-    //     renderTo: Ext.getBody(),
-    //     enableToggle: true
-    // });
-    var button = Ext.create('Ext.Button', {
-        text: 'Button',
-        margin: '-800 0 0 600',
-        renderTo: Ext.getBody(),
-        height: 90,
-        width: 140,
-        enableToggle: true,
-        items: {
-                xtype: 'button',
-                text: 'My tton',
-                badgeText: '2',
-                handler: function () {
-                    alert('tapped');
-                }
+            dataIndex: 'price',
+            field: {
+                xtype: 'textfield'
             }
+        }],
+        bbar: new Ext.PagingToolbar({
+            store: store,
+            displayInfo: true,
+            displayMsg: 'Показано  {0} - {1} из {2}'
+        }),
+        tbar: [
+            {
+                text: 'Insert',
+                handler: function () {
+                    store.insert(0, new Books());
+                    Editing.startEdit(0, 0);
+                }
+            },
+            {
+                itemId: 'delete',
+                text: 'Delete',
+                handler: function () {
+                    var selection = grid.getView().getSelectionModel().getSelection()[0];
+                    if (selection) {
+                        store.remove(selection);
+                    }
+                }
+            }],
+        renderTo: Ext.getBody()
     });
 });
