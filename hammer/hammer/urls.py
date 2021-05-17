f7d33b8d16a58b5d304368de8a019fed282e5759
Ivan Kinder <ivankinder11@gmail.com>
2021-05-17 03:40:23 +0300

    create token auth

--- hammer/hammer/urls.py
+++ hammer/hammer/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 from authapp.views import login, tmp_code, user, ReferalUserAPIView, obtain_auth_token
 
 router = DefaultRouter()
-# router.register('users', ReferalUserAPIView, basename='users')
+router.register('user', ReferalUserAPIView, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +14,6 @@ urlpatterns = [
     path('tmp_code/', tmp_code, name='tmp_code'),
     path('user/', user, name='user'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('api-auth/', login, name='auth'),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
 ]
