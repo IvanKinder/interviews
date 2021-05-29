75650d3b3d3fb42a1e0be775440d8735ad1f364c
Ivan Kinder <ivan.kinder@startinns.com>
2021-05-29 17:26:38 +0300

    beginning

---
+++ cinema/filmapp/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Film(models.Model):
+    name = models.CharField(max_length=32, verbose_name='название')
+    source = models.TextField(blank=True, verbose_name='ссылка на фильм')
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, verbose_name='фильм')
+    comment = models.TextField(blank=True, verbose_name='комментарий')
+
+
+class Star(models.Model):
+
+    count_of_stars = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, verbose_name='фильм')
+    stars = models.CharField(max_length=1, blank=True, choices=count_of_stars, verbose_name='количество звёздочек')
